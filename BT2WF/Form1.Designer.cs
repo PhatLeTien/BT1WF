af2507fe06ae444a674d4b1d45af1440adcaf574
PhatLeTien <tienphataki@gmail.com>
2024-04-10 10:16:36 +0700

    BT1WF

---
+++ BT2WF/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BT2WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.celsiusTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fahrenheitTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(199, 136);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(361, 35);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // celsiusTextBox
+            // 
+            this.celsiusTextBox.Location = new System.Drawing.Point(199, 96);
+            this.celsiusTextBox.Name = "celsiusTextBox";
+            this.celsiusTextBox.Size = new System.Drawing.Size(361, 20);
+            this.celsiusTextBox.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // fahrenheitTextBox
+            // 
+            this.fahrenheitTextBox.Location = new System.Drawing.Point(199, 188);
+            this.fahrenheitTextBox.Name = "fahrenheitTextBox";
+            this.fahrenheitTextBox.ReadOnly = true;
+            this.fahrenheitTextBox.Size = new System.Drawing.Size(361, 20);
+            this.fahrenheitTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.label1.Location = new System.Drawing.Point(242, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 36);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Celsius to Fahrenheit";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Celsius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fahrenheit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fahrenheitTextBox);
+            this.Controls.Add(this.celsiusTextBox);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox celsiusTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox fahrenheitTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
